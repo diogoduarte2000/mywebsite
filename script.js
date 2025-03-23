3cabbf3655978aeb8df1f541743a6a62c3bb3a85
diogoduarte2000 <diogo.silvanoduarte@gmail.com>
2025-03-23 14:16:53 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Configura o ScrollTrigger
-    gsap.registerPlugin(ScrollTrigger);
+    gsap.from(".sticky-header", { duration: 1, y: -100, opacity: 0, ease: "bounce" });
 
-    // Animação para cada seção com a classe .fade-in
     gsap.utils.toArray(".fade-in").forEach((section) => {
-        gsap.from(section, {
-            opacity: 0,
-            y: 50,
+        gsap.to(section, {
+            opacity: 1,
+            y: 0,
             duration: 1,
             scrollTrigger: {
                 trigger: section,
-                start: "top 80%", // Inicia a animação quando a seção está a 80% da parte superior da tela
-                toggleActions: "play none none none", // Play quando entra, nenhuma ação ao sair
+                start: "top 80%",
+                toggleActions: "play none none none"
             }
         });
     });
 });
+
