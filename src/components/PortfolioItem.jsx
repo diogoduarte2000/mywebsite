f3dc4e6dddfb6d45aa0aa0dc29ab35aeec7cc31b
Diogo Duarte <diogo.silvanoduarte@gmail.com>
2026-04-17 16:33:51 +0100

    Sync repository with current portfolio files

--- src/components/PortfolioItem.jsx
+++
@@ -1,8 +0,0 @@
-export const PortfolioItem = ({ href, bgColor, icon, text }) => (
-  <a href={href} className="portfolio-item">
-    <div className="portfolio-placeholder" style={{ backgroundColor: bgColor }}>
-      <i className={icon}></i>
-    </div>
-    <p>{text}</p>
-  </a>
-);
